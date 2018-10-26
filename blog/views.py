f6bb5ce22c6386e67dd1f8b01b89bf63b64a3f8c
zrtchp <zrtchp@qq.com>
2018-10-26 20:31:23 +0800

    download stati files,page 38

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import blogarticles
 
 def blog_title(request):
     blogs=blogarticles.objects.all()
     return render(request, 'blog/titles.html', {"blogs":blogs})
-# Create your views here.
+
 def blog_article(request,article_id):
-    article = blogarticles.objects.get(id=article_id)
+    #article = blogarticles.objects.get(id=article_id)
+    article = get_object_or_404(blogarticles,id=article_id)
     pub=article.publish
     return render(request, "blog/content.html", {"article":article, "publish":pub })
